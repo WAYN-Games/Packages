3a3bb7298592eeb8c6473b8f68cdd2f0b485e54a
WaynGames <WaynGames@users.noreply.github.com>
2026-04-03 11:56:25 +0000

    deploy: WAYN-Games/wayn.games.mgm.character-controller@60465f97b0daa22ee40173e42689c0a61094d7bd

---
+++ wayn.games.mgm.character-controller/v2.0.0/languages.js
@@ -0,0 +1,1 @@
+window.DOC_LANGUAGES = [{"code":"en","name":"English"},{"code":"fr","name":"Français"},{"code":"zh","name":"中文"}];
--- wayn.games.mgm.character-controller/versions.js
+++ wayn.games.mgm.character-controller/versions.js
@@ -1,1 +1,1 @@
-window.DOC_VERSIONS = [{"version":"0.6.0"},{"version":"0.5.0"},{"version":"0.4.2"},{"version":"0.4.1"},{"version":"0.4.0"},{"version":"0.3.0"}];
+window.DOC_VERSIONS = [{"version":"2.0.0"},{"version":"1.0.0"},{"version":"0.6.0"},{"version":"0.5.0"},{"version":"0.4.2"},{"version":"0.4.1"},{"version":"0.4.0"},{"version":"0.3.0"}];
